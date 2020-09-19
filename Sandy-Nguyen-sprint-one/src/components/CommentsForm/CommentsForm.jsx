219b69dbc37235b5b64778b0927d7b79851134d9
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-09-19 13:38:19 -0400

    Styling for Comments Form Tablet Done

--- Sandy-Nguyen-sprint-one/src/components/CommentsForm/CommentsForm.jsx
+++ Sandy-Nguyen-sprint-one/src/components/CommentsForm/CommentsForm.jsx
@@ -9,7 +9,7 @@ const CommentsForm = () => {
                     <div className="comments-section__photo"></div>
                     <form className="comments-section__form">
                         <label id="name" htmlFor="comment" className="comments-section__form-title">JOIN THE CONVERSATION</label>
-                        <br/><textarea type="text" className="comments-section__form-input comments-section__form-input-comment" name="comment" placeholder="Write comment here"></textarea>
+                        <br/><textarea type="text" className="comments-section__form-input" name="comment" placeholder="Write comment here"></textarea>
                         <button className="button comments-section__button" type="submit">COMMENT</button>
                     </form> 
             </div>
