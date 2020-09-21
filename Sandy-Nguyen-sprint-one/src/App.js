92c784fe8da1c8de4cbb0f1342a314dc09c14b6a
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-09-21 19:56:55 -0400

    Mobile Styling for Upload Page Complete

--- Sandy-Nguyen-sprint-one/src/App.js
+++ Sandy-Nguyen-sprint-one/src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header/Header';
 import HeroVideo from './components/HeroVideo/HeroVideo';
 import HeroVideoDetails from './components/VideoDetails/VideoDetails';
 import NextVideoQueue from './components/NextVideo/NextVideo';
 import CommentsSection from './components/CommentsSection/CommentsSection';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">   
-        <section className="header">
-            <Header />
-        </section>   
-        <HeroVideo />
-        <div className="section-container">
-            <section className="main-section">
-              <HeroVideoDetails />
-              <CommentsSection />
-            </section>
-            <section className="side-section">
-              <NextVideoQueue />
-            </section>
-        </div>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">   
+          <section className="header">
+              <Header />
+          </section>   
+          <HeroVideo />
+          <div className="section-container">
+              <section className="main-section">
+                <HeroVideoDetails />
+                <CommentsSection />
+              </section>
+              <section className="side-section">
+                <NextVideoQueue />
+              </section>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const API_KEY = '110f950a-c58f-42c6-969e-3e58a775af61';
+
+
+
--- Sandy-Nguyen-sprint-two/src/components/Upload/Upload.jsx
+++ Sandy-Nguyen-sprint-two/src/components/Upload/Upload.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import Header from '../Header/Header';
+import React from "react";
+import Header from "../Header/Header";
+import UploadImage from "../../assets/images/Upload-video-preview.jpg";
+import "./upload.scss";
 
 function Upload() {
     return (
-        <Header />
+        <>
+            <Header />
+            <div className="divider upload-section__divider--thin"></div>
+            <div className="upload-section">
+                <h1 className="upload-section__heading">Upload Video</h1>
+                <div className="divider upload-section__divider upload-section__divider--hidden"></div>
+                <div className="upload-section__video">
+                    <h4 className="upload-section__video-title">VIDEO THUMBNAIL</h4>
+                    <img src={UploadImage} className="upload-section__video-thumbnail" alt="Bicycle Thumbnail"/>
+                </div>
+                <div className="upload-section__form-container">
+                    <form className="upload-section__form">
+                        <label className="upload-section__form-title">TITLE YOUR VIDEO</label>
+                        <br/><input type="text" className="upload-section__form-input upload-section__form-input-title" placeholder="Add a title to your video"/>
+                        <br/><label className="upload-section__form-title">ADD A VIDEO DESCRIPTION</label>
+                        <br/><textarea type="text" className="upload-section__form-input upload-section__form-input-description" placeholder="Add a description of your video"/>
+                        <div className="divider upload-section__divider upload-section__divider--hidden"></div>
+                        <div className=" upload-section__form-options"></div>
+                            <button className="upload-section__form-options-publish">PUBLISH</button>
+                            <h3 className="upload-section__form-options-cancel">CANCEL</h3>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
