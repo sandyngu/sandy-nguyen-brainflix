b7f6941b07bdf05a3a987379bf9220a709e7ef21
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-09-22 00:48:56 -0400

    Styling for Desktop Uploads Page Complete

--- Sandy-Nguyen-sprint-two/src/components/Upload/Upload.jsx
+++ Sandy-Nguyen-sprint-two/src/components/Upload/Upload.jsx
@@ -11,23 +11,25 @@ function Upload() {
             <div className="upload-section">
                 <h1 className="upload-section__heading">Upload Video</h1>
                 <div className="divider upload-section__divider upload-section__divider--hidden"></div>
-                <div className="upload-section__video">
-                    <h4 className="upload-section__video-title">VIDEO THUMBNAIL</h4>
-                    <img src={UploadImage} className="upload-section__video-thumbnail" alt="Bicycle Thumbnail"/>
+                <div className="upload-section__video-form-container">
+                    <div className="upload-section__video">
+                        <h4 className="upload-section__video-title">VIDEO THUMBNAIL</h4>
+                        <img src={UploadImage} className="upload-section__video-thumbnail" alt="Bicycle Thumbnail"/>
+                    </div>
+                    <div className="upload-section__form-container">
+                        <form className="upload-section__form">
+                            <label className="upload-section__form-title">TITLE YOUR VIDEO</label>
+                            <br/><input type="text" className="upload-section__form-input upload-section__form-input-title" placeholder="Add a title to your video"/>
+                            <br/><label className="upload-section__form-title">ADD A VIDEO DESCRIPTION</label>
+                            <br/><textarea type="text" className="upload-section__form-input upload-section__form-input-description" placeholder="Add a description of your video"/>
+                        </form>
+                    </div>
                 </div>
-                <div className="upload-section__form-container">
-                    <form className="upload-section__form">
-                        <label className="upload-section__form-title">TITLE YOUR VIDEO</label>
-                        <br/><input type="text" className="upload-section__form-input upload-section__form-input-title" placeholder="Add a title to your video"/>
-                        <br/><label className="upload-section__form-title">ADD A VIDEO DESCRIPTION</label>
-                        <br/><textarea type="text" className="upload-section__form-input upload-section__form-input-description" placeholder="Add a description of your video"/>
-                        <div className="divider upload-section__divider upload-section__divider--hidden"></div>
-                        <div className=" upload-section__form-options">
-                            <button className="upload-section__form-options-publish">PUBLISH</button>
-                            <h3 className="upload-section__form-options-cancel">CANCEL</h3>
-                        </div>
-                    </form>
-                </div>
+                    <div className="divider upload-section__divider upload-section__divider--hidden"></div>
+                    <div className=" upload-section__form-options">
+                        <button className="upload-section__form-options-publish">PUBLISH</button>
+                        <h3 className="upload-section__form-options-cancel">CANCEL</h3>
+                    </div>
             </div>
         </>
     )
